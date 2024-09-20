a7728062bdeba2ccb6e052edc7cd59564f80d1af
Henry Doan <henrydoan96@gmail.com>
2024-09-19 21:47:01 -0600

    scroll to top

--- client/src/components/shared/Dash.js
+++ client/src/components/shared/Dash.js
@@ -1,8 +1,11 @@
+import * as Icon from 'react-bootstrap-icons';
+import ScrollToTop from "react-scroll-to-top";
 import Jobapps from "../jobapps/Jobapps";
 
 const Dash = () => (
   <>
     <Jobapps />
+    <ScrollToTop smooth component={<Icon.ArrowUpShort />} />
   </>
 )
 
